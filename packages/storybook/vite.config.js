b6922d4248ab1070896dc34922e86d24e4b8fb35
Felipe Bambirra <lipex360@gmail.com>
2022-10-15 16:31:39 -0300

    storybook added

---
+++ packages/storybook/vite.config.js
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+})
